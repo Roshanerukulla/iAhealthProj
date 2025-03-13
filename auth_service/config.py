ae52cc2edb1eaa679482ccb30aaad020d03aa000
Roshan erukulla <roshanerukulla05@gmail.com>
2025-03-13 14:26:19 -0400

    updated config

--- auth_service/config.py
+++ auth_service/config.py
@@ -6,5 +6,5 @@ class Config:
     password = "Gupta@4k4"
     encoded_password = urllib.parse.quote_plus(password)
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{encoded_password}@localhost/auth_db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")  # For session management
